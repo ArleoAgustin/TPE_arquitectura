1fdde13351ae32fffdaa79aaab86b6ab32165cb4
Agustin Arleo <arleo.agustin20@gmail.com>
2023-10-24 16:24:01 -0300

    Initial commit

---
+++ src/main/java/tpe/tpe_arquitectura/TpeArquitecturaApplication.java
@@ -0,0 +1,13 @@
+package tpe.tpe_arquitectura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpeArquitecturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpeArquitecturaApplication.class, args);
+	}
+
+}
---
+++ stoppingService/src/test/java/service/stopping/StoppingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package service.stopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoppingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
