f5b0472e2d4c3842392f30eb22fd8e77a1eff79a
Lulo <lulobareiss@gmail.com>
2023-10-27 12:24:57 -0300

    base model data structure from travel/billing

---
+++ travelService/src/main/java/app/model/classes/User.java
@@ -0,0 +1,72 @@
+package app.model.classes;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class User implements Serializable {
+    private long DNI;
+
+    private Long dni;
+    private String name;
+    private String lastName;
+    private Long numberPhone;
+    private String email;
+    private String startDate;
+    private Character state;
+
+    public User(Long dni, String name, String lastName, Long numberPhone, String email, String startDate,Character state) {
+        this.dni = dni;
+        this.name = name;
+        this.lastName = lastName;
+        this.numberPhone = numberPhone;
+        this.email = email;
+        this.startDate = startDate;
+        this.state=state;
+    }
+
+    public User(User user) {}
+
+    public Long getDni() {
+        return dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Long getNumberPhone() {
+        return numberPhone;
+    }
+
+    public void setNumberPhone(Long numberPhone) {
+        this.numberPhone = numberPhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public Character getState() {
+        return state;
+    }
+
+}
---
+++ travelService/src/main/java/app/model/entities/Travel.java
@@ -0,0 +1,95 @@
+package app.model.entities;
+
+import app.model.classes.User;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+@Data
+@Entity
+public class Travel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private LocalDateTime date;
+    @Column(nullable = false)
+    private LocalDateTime start;
+    @Column
+    private LocalDateTime end;
+    @Column
+    private Double kmTraveled;
+    @Column
+    private Double tariff;
+    @Column
+    private Long userDNI;
+    @Column
+    private Long scooterID;
+
+    public Travel(Double tariff, Long userDNI, Long scooterID) {
+        this.start = LocalDateTime.now();
+        this.kmTraveled = 0.0;
+        this.tariff = tariff;
+        this.userDNI = userDNI;
+        this.scooterID = scooterID;
+    }
+
+    public Travel(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+
+    public Double getKmTraveled() {
+        return kmTraveled;
+    }
+
+    public void setKmTraveled(Double kmTraveled) {
+        this.kmTraveled = kmTraveled;
+    }
+
+    public Double getTariff() {
+        return tariff;
+    }
+
+    public void setTariff(Double tariff) {
+        this.tariff = tariff;
+    }
+
+    public Long getUserDNI() {
+        return userDNI;
+    }
+
+    public Long getScooterID() {
+        return scooterID;
+    }
+
+}
